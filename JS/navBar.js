8c38b414e0a31aea6c53bb7b91d370b4684977ba
Paulino8099 <dapa7928@gmail.com>
2022-02-08 09:10:32 -0400

    1.1

--- JS/navBar.js
+++ JS/navBar.js
@@ -1,31 +1,61 @@
 
 
-let sections = document.documentElement.querySelectorAll('.section');
+navBarBtn();
+function navBarBtn() {
+    let navBar = document.documentElement.querySelector('.navBar');
+    let btnNavBar = document.documentElement.querySelector('.btn-open-navBar');
+    let home = document.documentElement.querySelector('.home');
 
-const visor = (entry, salida) => {
-    // var btns
-    let btnHome = document.documentElement.querySelector('.btn-home-navBar');
-    let btnAbout = document.documentElement.querySelector('.btn-about-navBar');
-    let btnExperiences = document.documentElement.querySelector('.btn-experiences-navBar');
-    let btnContact = document.documentElement.querySelector('.btn-contact-navBar');
+    /**
+     * ?desktop
+     */
+    if (document.documentElement.scrollWidth > 750) {
+        btnNavBar.classList.add('activeDesktop');
 
-    entry.forEach(function (entry) {
-        if (entry.isIntersecting) {
-            /**
-             * ?home...
-             */
-            if (entry.target.classList == 'home section') {
-                btnHome.classList.add('active');
-            }
-        };
+        btnNavBar.addEventListener('click', function () {
+            btnNavBar.classList.toggle('activeDesktop');
+            navBar.classList.toggle('activeDesktop');
+            home.classList.toggle('activeDesktop');
+        });
+    }
+    /**
+     * ?mobile
+     */
+    btnNavBar.addEventListener('click', function () {
+        btnNavBar.classList.toggle('active');
+        navBar.classList.toggle('active');
     });
 };
 
-const observador = new IntersectionObserver(visor, {
-    root: null, 
-    rootMargin: "-50%"
-});
+observadorSection();
+function observadorSection() {
+    let sections = document.documentElement.querySelectorAll('.section');
 
-sections.forEach(e => {
-    observador.observe(e);
-})
+    const visor = (entry, salida) => {
+        // var btns
+        let btnHome = document.documentElement.querySelector('.btn-home-navBar');
+        let btnAbout = document.documentElement.querySelector('.btn-about-navBar');
+        let btnExperiences = document.documentElement.querySelector('.btn-experiences-navBar');
+        let btnContact = document.documentElement.querySelector('.btn-contact-navBar');
+
+        entry.forEach(function (entry) {
+            if (entry.isIntersecting) {
+                /**
+                 * ?home...
+                 */
+                if (entry.target.classList == 'home section') {
+                    btnHome.classList.add('active');
+                }
+            };
+        });
+    };
+
+    const observador = new IntersectionObserver(visor, {
+        root: null,
+        rootMargin: "-50%"
+    });
+
+    sections.forEach(e => {
+        observador.observe(e);
+    });
+};
