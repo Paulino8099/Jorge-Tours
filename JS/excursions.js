ada9c274bca4ac013d44413414be1c9b1ce91430
Paulino8099 <dapa7928@gmail.com>
2022-02-10 21:42:50 -0400

    1.8

---
+++ JS/excursions.js
@@ -0,0 +1,10 @@
+let PR = document.documentElement.querySelector('.details-PR');
+
+let contatorSpecial = 0;
+
+document.log(PR.scrollTop())
+
+// while (contatorSpecial <= PR.) {
+    
+// }
+PR.scrollTop = contatorSpecial;
