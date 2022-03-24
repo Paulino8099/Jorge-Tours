efc83f49d30157fbeba893f489830a2a162bb002
Paulino8099 <dapa7928@gmail.com>
2022-03-24 19:27:31 -0400

    v4.0

--- JS/excursions.js
+++ JS/excursions.js
@@ -1,24 +1,31 @@
 
 /**
+ * *variables globales
+ */
+let accountant = 0;
+let listImg;
+
+/**
  * *datos flotantes
  */
 // datos flotantes de las imagenes principales "3medias"
 dataFloat();
 function dataFloat() {
     let targetAllImg = document.documentElement.querySelectorAll('.click');
-    let mediaExcursions = document.documentElement.querySelector('.medias-excursions'); //container excurisions
-    let btnCancel = document.documentElement.querySelector('.btn-medias-excursions');
+    let mediaExcursions = document.documentElement.querySelector('.fullscreen'); //container excurisions
+    let btnCancel = document.documentElement.querySelector('.btn-close-fullscreen');
     let backdropBox = document.documentElement.querySelector('.backdrop-box');
     let body = document.documentElement.querySelector('body');
 
     /**
-     * *agregando eventos a cada imágen
-     *///agregando eventos o funciones a cada imagen en particular de las tarjetas del contenedor "excursions"
+     * +agregando eventos a cada imágen
+     */
     targetAllImg.forEach((e) => {
+        //agregando eventos o funciones a cada imagen en particular de las tarjetas del contenedor "excursions"
         e.addEventListener('click', function (e) {
-            let img = document.documentElement.querySelector('.img-medias-excursions');
-            let title = document.documentElement.querySelector('.textTitle-medias-excursions');
-            let description = document.documentElement.querySelector('.text-des-medias-excursions');
+            let img = document.documentElement.querySelector('.img-fullscreen');
+            let title = document.documentElement.querySelector('.textTitle-fullscreen');
+            let description = document.documentElement.querySelector('.text-des-fullscreen');
 
             /**
              * ?activando backdrop y dataFloat
@@ -177,8 +184,6 @@ function openIncluded() {
  */
 allConfigExperiences();
 function allConfigExperiences() {
-    let accountant;
-    let listImg;
     let btnBack;
     let btnNext;
     let experiencesContainer;
@@ -214,9 +219,15 @@ function allConfigExperiences() {
                  */
                 openContainerExperiences();
                 function openContainerExperiences() {
-                    // abriendo contenedor "experiencias" de la tarjeta a la que se le dió click.
 
-                    e.target.parentNode.parentNode.parentNode.children[4].classList.toggle('active');
+                    // abriendo contenedor "experiencias" de la tarjeta a la que se le dió click.
+                    if (e.target.innerHTML == 'Experiencias') {
+                        e.target.parentNode.parentNode.parentNode.children[4].classList.toggle('active');
+                    }
+                    // abriendo contenedor "experiencias" de la tarjeta a la que se le dió click.
+                    else if (e.target.innerHTML == '<h3>Experiencias</h3>') {
+                        e.target.parentNode.parentNode.children[4].classList.toggle('active');
+                    }
                 };
             });
         });
@@ -253,8 +264,6 @@ function allConfigExperiences() {
                  */
                 // lista de todas las imagenes concernientes a la tarjeta que se le hizo click en "experiencias"
                 listImg = e.target.parentNode.parentNode.parentNode.children[4].children[0].children[2].children;
-                // contador
-                accountant = 0;
                 // seleccionando btn back de c/u de las tarjetas
                 btnBack = e.target.parentNode.parentNode.parentNode.children[4].children[0].children[0].children[1];
                 // seleccionando btn next de c/u de las tarjetas
@@ -276,27 +285,6 @@ function allConfigExperiences() {
                  */
 
                 /**
-                 * +on/off btns navigation
-                 */
-                // activando o desactivando los botones de navegación. Esto solo va a suceder en caso de que ya 
-                //no hayan mas imagenes que mostrar al retroceder o adelantar
-                onOffBtns();
-                function onOffBtns() {
-                    /**
-                     * ?on/off btn "back"
-                     */
-                    if (accountant == 0) {
-                        btnBack.classList.toggle('desactive');
-                    }
-                    /**
-                     * ?on/off btn "next"
-                     */
-                    if (accountant == listImg.length - 1) {
-                        btnNext.classList.toggle('desactive');
-                    }
-                }
-
-                /**
                  * +navegando con btns (left, back)
                  */
                 btnLeftBack();
@@ -305,7 +293,7 @@ function allConfigExperiences() {
                     btnBack.addEventListener('click', btnBack2);
                     //navegando hacia adelante entre las imagenes
                     btnNext.addEventListener('click', btnNext2);
-                    
+
                     // removiendo eventListener del btn 
                     btnsExperiences.forEach((e) => {
                         e.addEventListener('click', function () {
@@ -313,7 +301,7 @@ function allConfigExperiences() {
                             btnNext.removeEventListener('click', btnNext2);
                         });
                     });
-                    
+
                     function btnBack2() {
                         if (accountant >= 1) {
                             accountant -= 1;
@@ -326,12 +314,132 @@ function allConfigExperiences() {
                         if (accountant <= (listImg.length - 2)) {
                             accountant += 1;
                         };
-                        
+
                         refillingIMg();
                         onOffBtns();
                     };
+                    /**
+                     * +on/off btns navigation
+                     */
+                    // activando o desactivando los botones de navegación. Esto solo va a suceder en caso de que ya 
+                    //no hayan mas imagenes que mostrar al retroceder o adelantar
+                    onOffBtns();
+                    function onOffBtns() {
+                        let btnRight = document.querySelectorAll('.fa-solid fa-angle-right');
+                        let btnLeft = document.querySelectorAll('.fa-solid fa-angle-left');
+
+                        /**
+                         * ?on/off btn "back"
+                         */
+                        if (accountant == 0) {
+                            btnRight.forEach((e) => {
+                                console.log(e.target);
+                            });
+                        }
+                        /**
+                         * ?on/off btn "next"
+                         */
+                        if (accountant == listImg.length - 1) {
+                            btnNext.style.color = '#ff0000';
+                        }
+                    }
                 };
             });
         });
     };
+    /**
+     * ?abriendo imágen en grande
+     */
+    imgBig();
+    function imgBig() {
+        let mediaExcursions = document.documentElement.querySelector('.fullscreen'); //container excurisions
+        let btnRightScreen = document.querySelector('.btn-right-fullscreen');
+        let btnCloseScreen = document.documentElement.querySelector('.fullscreen .btn-close-fullscreen');
+        let btnLeftScreen = document.querySelector('.btn-left-fullscreen');
+        let backdropBox = document.documentElement.querySelector('.backdrop-box');
+        let description = document.documentElement.querySelector('.text-des-fullscreen');
+        let btnBigImg = document.querySelectorAll('.wraper-excursions .fa-expand');
+        let title = document.documentElement.querySelector('.textTitle-fullscreen');
+        let body = document.documentElement.querySelector('body');
+        let img = document.documentElement.querySelector('.img-fullscreen');
+        let updateImgExperiences;
+
+        btnBigImg.forEach((e) => {
+            e.addEventListener('click', function (e) {
+                updateImgExperiences = e.target.parentNode.parentNode.parentNode.children[1];
+                
+                //activando el backdrop y la ventana flotante para mostrar la foto en grande
+                mediaExcursions.classList.add('active');
+                backdropBox.classList.add('active');
+                body.classList.add('active');
+
+                /**
+                 * +navegando con btns (left, back)
+                 */
+                btnLeftBack();
+                function btnLeftBack() {
+                    //navegando hacia adelante entre las imagenes
+                    btnLeftScreen.addEventListener('click', function () {
+                        btnLeft2();
+                    });
+                    //navegando hacia atrás entre las imagenes
+                    btnRightScreen.addEventListener('click', function () {
+                        btnRight2();
+                    });
+
+                    // removiendo eventListener de los btns de navegación al salir de la ventana "fullScreen"
+                    btnCloseScreen.addEventListener('click', function () {
+                        btnLeftScreen.removeEventListener('click', btnLeft2);
+                        btnRightScreen.removeEventListener('click', btnRight2);
+                    });
+                    // removiendo eventListener de los btns de navegación al dar click en la ventana "backDrop"
+                    backdropBox.addEventListener('click', function () {
+                        btnLeftScreen.removeEventListener('click', btnLeft2);
+                        btnRightScreen.removeEventListener('click', btnRight2);
+                    });
+
+                    function btnLeft2() {
+                        if (accountant >= 1) {
+                            accountant -= 1;
+                        };
+
+                        refilling();
+                    };
+                    function btnRight2() {
+                        if (accountant <= (listImg.length - 2)) {
+                            accountant += 1;
+                        };
+
+                        refilling();
+                    };
+                };
+
+                // rellenando datos de imágen en grande y actualizando la imágen de "experiencias".
+                refilling();
+                function refilling() {
+                    // rellenando datos del bg del fondo.
+                    backdropBox.style.background = `url(${listImg[accountant].src})`;
+                    backdropBox.style.backgroundSize = 'cover';
+                    backdropBox.style.backgroundPosition = 'center';
+                    backdropBox.style.backgroundRepeat = 'no-repeat';
+
+                    // rellenando imágen de ventana en grande.
+                    img.innerHTML = `<img src="${listImg[accountant].src}" alt="">`;
+
+                    // actualizando imágen del contenedor "experiencias" al ir navegando entre las 
+                    // imagenes de "fullScreen".
+                    updateImgExperiences.innerHTML = `<img src="${listImg[accountant].src}" alt="">`;
+                };
+
+                // rellenando datos del título de la ventana flotante.
+                title.innerHTML = '';
+                // rellenando los datos de la descrición de la ventana flotante.
+                description.innerHTML = '';
+            });
+        });
+    };
 };
+
+
+
+                // console.log(e.target.parentNode.parentNode.parentNode.children[1]);
